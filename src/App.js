7322783f881e44a295421d9e5f27e5e5c594d8d1
priyansh <priyanshv03@gmail.com>
2022-04-28 14:25:18 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 // import Football from './component/Football';
-// import Netflix from './component/Netflix';
+import Netflix from './component/Netflix';
 // import Thapa1 from './component/Thapa1';
 // import Thapa2 from './component/Thapa2';
 // import Thapa3 from './component/Thapa3';
 
 function App() {
   return (
-    <>
+    <>n
 {/* <Thapa1/> */}
 {/* <Thapa2/> */}
 {/* <Thapa3/> */}
-{/* <Netflix/> */}
+<Netflix/>
 {/* <Football/> */}
 
+      
+
+
+
+      
   </>
   );
 }
